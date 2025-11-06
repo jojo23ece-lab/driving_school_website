36eb13d7b6ac0b1555f750b43ebfcbf56bc7276b
jojo23ece-lab <jojo23ece@cmrit.ac.in>
2025-11-06 18:48:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -158,7 +158,8 @@ if selected== 'blog':
 if selected=='mock test':
     st.title('video')
     st.header("Mock Driving Test Video")
-    st.video(https://youtu.be/oW6m7fABIGs?si=eM4VoTdp35j0Zolv)
+    st.video('https://youtu.be/oW6m7fABIGs?si=eM4VoTdp35j0Zolv')
+
 
 
 
